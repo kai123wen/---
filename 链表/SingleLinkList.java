9adc7141e24b50880ec0fb38a0e88c7e7354d733
kai123wen <1819306561@qq.com>
2019-08-05 21:03:57 +0800

    提交实现两个有序链表合并的代码

--- 链表/SingleLinkList.java
+++ 链表/SingleLinkList.java
@@ -7,16 +7,10 @@ package LinkList;
  * @class_name: SingleLinkList
  * @class_describe: 实现单链表，支持增删操作
  * @establish_time: 2019年8月4日 下午9:15:58
- * @how_to_use: 
- * SingleLinkList link = new SingleLinkList(); 
- * for (int i = 0; i < 5; i++) { 
- * 		link.addNode(i); 
- * } 
- * link.printLinkList();
- * link.deleteNode(2);
- * link.printLinkList();
- * link.reverseLinkList();
-	link.printLinkList();
+ * @how_to_use: SingleLinkList link = new SingleLinkList(); for (int i = 0; i <
+ *              5; i++) { link.addNode(i); } link.printLinkList();
+ *              link.deleteNode(2); link.printLinkList();
+ *              link.reverseLinkList(); link.printLinkList();
  */
 public class SingleLinkList {
 	class Node {
@@ -69,6 +63,62 @@ public class SingleLinkList {
 			}
 			length--;
 		}
+	}
+
+	/*
+	 * 方法目的是得到头节点，为了合并两个单链表用
+	 */
+	public Node getHeadNode() {
+		return head;
+	}
+
+	public void setHeadNode(Node head) {
+		this.head = head;
+	}
+
+	/*
+	 * 实现两个有序的链表合并为一个有序链表
+	 */
+	public SingleLinkList merge(SingleLinkList linkOne, SingleLinkList linkTwo) {
+		Node headNew = null; //指向新的链表头节点
+		Node endNew = null; // 指向新的链表尾节点
+		Node linkNodeOne = linkOne.getHeadNode(); //获取到第一个链表
+		Node linkNodeTwo = linkTwo.getHeadNode(); //获取到第二个链表
+		while (linkNodeOne != null && linkNodeTwo != null) {
+			Node node = new Node();
+			if (linkNodeOne.data < linkNodeTwo.data) {
+				node.data = linkNodeOne.data;
+				linkNodeOne = linkNodeOne.next;
+			} else {
+				node.data = linkNodeTwo.data;
+				linkNodeTwo = linkNodeTwo.next;
+			}
+			if (headNew == null) {
+				headNew = node;
+				endNew = node;
+			} else {
+				endNew.next = node;
+				endNew = node;
+			}
+		}
+		while (linkNodeOne != null) {
+			Node node = new Node();
+			node.data = linkNodeOne.data;
+			endNew.next = node;
+			endNew = node;
+			linkNodeOne = linkNodeOne.next;
+		}
+
+		while (linkNodeTwo != null) {
+			Node node = new Node();
+			node.data = linkNodeTwo.data;
+			endNew.next = node;
+			endNew = node;
+			linkNodeTwo = linkNodeTwo.next;
+		}
+		SingleLinkList newLink = new SingleLinkList();
+		newLink.setHeadNode(headNew);
+		return newLink;
 	}
 
 	/*
