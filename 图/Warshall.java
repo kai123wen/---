6aa647df6eb8d68b32a9b349d98ce92587c08dae
kai123wen <1819306561@qq.com>
2019-09-26 15:09:00 +0800

    实现 warshall 来找有向图的闭包的代码

---
+++ 图/Warshall.java
@@ -0,0 +1,49 @@
+package Graph;
+
+import java.io.PrintStream;
+
+public class Warshall {
+	public static void main(String[] args) {
+		warshall();
+	}
+
+	public static void warshall() {
+		int row = 5;
+		int column = 5;
+		int[][] graphMatrix = new int[row][column];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				graphMatrix[i][j] = 0;
+			}
+		}
+		graphMatrix[0][1] = 1;
+		graphMatrix[1][2] = 1;
+		graphMatrix[2][3] = 1;
+		graphMatrix[3][0] = 1;
+		graphMatrix[3][4] = 1;
+
+		for (int n = 0; n < row; n++) {
+			// m 指向的是十字的行，k指向的是十字的列
+			for (int m = 0; m < row; m++) {
+				for (int k = 0; k < row; k++) {
+					if (m == n) {
+						break;
+					}
+					if (graphMatrix[n][m] == 1 && graphMatrix[k][n] == 1) {
+						graphMatrix[k][m] = 1;
+					}
+				}
+			}
+		}
+		printArray(graphMatrix, 5, 5);
+	}
+
+	public static void printArray(int[][] array, int row, int column) {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
